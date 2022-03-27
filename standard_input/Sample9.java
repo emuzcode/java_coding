426fad6c7c1c97f11280a361e6325b4ead3e86e2
Taro Sakazume <ts95.business@gmail.com>
2022-03-27 15:44:16 +0900

    add standard_input/Sample9

---
+++ standard_input/Sample9.java
@@ -0,0 +1,16 @@
+// Choose from a pair of strings and integers
+
+class Sample9 {
+  public static void main(String[] args) {
+    int N = 5;
+    String[] a = { "a", "b", "c", "d", "e" };
+    int[] b = { 1, 2, 3, 4, 5 };
+
+    System.out.println(a[3] + " " + b[3]);
+  }
+}
+
+/*
+ * >>java Sample9
+ * d 4
+ */
